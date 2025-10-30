c873947c574adef82890c44f19a2b71b46ec83c4
sridhar12345-msajce <311823205045@msajce-edu.in>
2025-10-30 15:07:04 +0530

    Create oddeve.java

---
+++ oddeve.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Oddeve {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("number is even" );
+        }
+        else{
+            System.out.println("number is odd" );
+        }
+
+    }
+}
