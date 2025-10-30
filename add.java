448c603fad6fa7c585f42a597c795b6f65202344
sridhar12345-msajce <311823205045@msajce-edu.in>
2025-10-30 15:13:51 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,14 @@
+public class add {
+     int a=10;
+     int b=20;
+     int result;
+     public static void main(String[] args) {
+         add obj=new add();
+         obj.a=10;
+         obj.b=20;
+         obj.result=obj.a+obj.b;
+         System.out.println(obj.result);
+
+
+     }
+}
