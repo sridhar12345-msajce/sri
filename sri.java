447904d433d545a88c22d3d3a462ee8a7870de9b
sridhar12345-msajce <311823205045@msajce-edu.in>
2025-10-30 15:06:05 +0530

    Create sri.java

---
+++ sri.java
@@ -0,0 +1,16 @@
+public class Jadx {
+    public  static void msn(String a ,String c){
+
+
+        String b = "technologies ";
+
+        String d = a + b + c;
+        System.out.println(d);
+    }
+    public static void main(String[] args){
+        String a ="jadx ";
+        String c = "software ";
+        msn(a,c);
+
+    }
+}
