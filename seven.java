e919e0c6a83346c40795a71e86fa8b6769701e42
sridhar12345-msajce <311823205045@msajce-edu.in>
2025-10-30 15:14:46 +0530

    Create seven.java

---
+++ seven.java
@@ -0,0 +1,15 @@
+public class seventh {
+
+        public static void main(String[] args) {
+
+            char ch1 = 'x';
+            char ch2 = 'a';
+
+
+            if (ch1 < ch2) {
+                System.out.println(ch1 + ", " + ch2);
+            } else {
+                System.out.println(ch2 + ", " + ch1);
+            }
+        }
+    }
